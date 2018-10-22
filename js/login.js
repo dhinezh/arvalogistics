17a02c4c0f228a38499743e58fb900041df20c9e
Dhinesh V.S <s.dhinezh@gmail.com>
2018-10-22 23:42:59 +0530

    login changes

---
+++ js/login.js
@@ -0,0 +1,44 @@
+jQuery(function($) {
+  'use strict';
+
+  /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+
+  $(function() {
+    $('#status').fadeOut();
+    $('#preloader').delay(200).fadeOut('slow');
+  });
+
+  // -----------------------------------------------------------------
+  // Login FORM
+  // ------------------------------------------------------------------
+
+  (function() {
+    $('#loginForm').on('submit', function(e) {
+      e.preventDefault();
+
+      var $action = $(this).prop('action');
+      var $data = $(this).serialize();
+      var $this = $(this);
+
+      $this.prevAll('.alert').remove();
+
+      $.post(
+        $action,
+        $data,
+        function(data) {
+          if (data.response == 'error') {
+            $this.before('<div class="alert alert-danger">' + data.message + '</div>');
+          }
+
+          if (data.response == 'success') {
+            $this.before('<div class="alert alert-success">' + data.message + '</div>');
+            $this.find('input, textarea').val('');
+          }
+        },
+        'json'
+      );
+    });
+  })();
+});
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    session_cache_limiter( 'nocache' );
+    header( 'Expires: ' . gmdate( 'r', 0 ) );
+    header( 'Content-type: application/json' );
+
+    $username   = strip_tags($_POST['username']);
+    $password   = strip_tags($_POST['password']);
+    $result     = array();
+
+
+    if(empty($username)){
+
+        $result = array( 'response' => 'error', 'empty'=>'username', 'message'=>'<strong>Error!</strong> Username is empty.' );
+        echo json_encode($result );
+        die;
+    }
+
+    if(empty($password)){
+
+        $result = array( 'response' => 'error', 'empty'=>'password', 'message'=>'<strong>Error!</strong> password is empty.' );
+        echo json_encode($result );
+        die;
+    }
+
+    if ( ) {
+        $result = array( 'response' => 'success', 'message'=>'<strong>Login sucessfull!</strong>' );
+    } else {
+        $result = array( 'response' => 'error', 'message'=>'<strong>Error!</strong> Username/password is wrong.'  );
+    }
+
+    echo json_encode( $result );
+
+    die;
+?>
